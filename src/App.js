084c56ffded26befc056c5c81babcf4846ea4a77
ShocK-100 <talzvi99@gmail.com>
2022-10-30 21:53:45 +0200

    api call works

--- src/App.js
+++ src/App.js
@@ -5,24 +5,96 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
 import "./App.css";
+import { Component } from "react";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg
-        className="particles"
-        type="cobweb"
-        bg={true}
-        num={100}
-        color="#ffffff"
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition />  */}
-    </div>
-  );
+// const Clarifai = require("clarifai");
+const USER_ID = "tal";
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = "830f6947a77f40449fed31e2ca59ef80";
+const APP_ID = "my-first-application";
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = "color-recognition";
+const MODEL_VERSION_ID = "dd9458324b4b45c2be1a7ba84d27cd04";
+const IMAGE_URL = "https://samples.clarifai.com/face-det.jpg";
+let raw = JSON.stringify({
+  user_app_id: {
+    user_id: USER_ID,
+    app_id: APP_ID,
+  },
+  inputs: [
+    {
+      data: {
+        image: {
+          url: IMAGE_URL,
+        },
+      },
+    },
+  ],
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    Authorization: "Key " + PAT,
+  },
+  body: raw,
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageURL: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageURL: this.state.input });
+    let obj = JSON.parse(raw);
+    obj["inputs"][0]["data"]["image"]["url"] = this.state.imageURL;
+    raw = JSON.stringify(obj);
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        MODEL_ID +
+        "/versions/" +
+        MODEL_VERSION_ID +
+        "/outputs",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg
+          className="particles"
+          type="cobweb"
+          bg={true}
+          num={100}
+          color="#ffffff"
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL} />
+      </div>
+    );
+  }
 }
 
 export default App;
